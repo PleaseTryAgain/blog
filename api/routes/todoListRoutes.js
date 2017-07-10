0e65a415a143ec9ba600aa5702b7d082e03ddb23
paprika zhou <PleaseTryAgain@users.noreply.github.com>
2017-07-10 10:08:06 +0800

    Update todoListRoutes.js

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -2,12 +2,12 @@
 module.exports = function(app) {
 	var todoList = require("../controllers/todoListController")
 
-	// app.router('/tasks')
-		app.get('/tasks', todoList.list_all_tasks)
-		app.post('/tasks', todoList.create_a_task);
+	app.route('/tasks')
+		app.get(todoList.list_all_tasks)
+		app.post(todoList.create_a_task);
 
-	// app.router('/task/:taskId')
-		app.get('/task/:taskId', todoList.read_a_task)
-		app.put('/task/:taskId', todoList.update_a_task)
-		app.delete('/task/:taskId', todoList.delete_a_task);
+	app.route('/task/:taskId')
+		app.get(todoList.read_a_task)
+		app.put(todoList.update_a_task)
+		app.delete(todoList.delete_a_task);
 };
